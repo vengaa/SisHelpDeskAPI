0e6d24472a004eb8e2dbeb2cb436c12b9033bf57
vengaa <luisfelipeadm.ps@gmail.com>
2024-12-28 14:52:38 -0300

    feat: project created

---
+++ src/main/java/me/felipedev/sishelpdesk/SisHelpDeskApiApplication.java
@@ -0,0 +1,13 @@
+package me.felipedev.sishelpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisHelpDeskApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisHelpDeskApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/felipedev/sishelpdesk/SisHelpDeskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.felipedev.sishelpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisHelpDeskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
